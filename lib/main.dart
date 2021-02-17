9305e7f63f32ef8875eba2df3e3c4095ca9a13a2
Shane Blume <shanesblume@gmail.com>
2021-02-17 07:58:52 -0500

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,106 @@
+import 'package:budget/Transaction/add_transaction.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(App());
+}
+
+class App extends StatefulWidget {
+  _AppState createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  bool _initialized = false;
+  bool _error = false;
+
+  void initializeFlutterFire() async {
+    try {
+      await Firebase.initializeApp();
+      setState(() {
+        _initialized = true;
+      });
+    } catch (e) {
+      setState(() {
+        _error = true;
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    initializeFlutterFire();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (_error) {
+      return somethingWentWrong();
+    }
+
+    if (!_initialized) {
+      return loading();
+    }
+
+    return budgetApp();
+  }
+
+  Widget somethingWentWrong() {
+    return Container(
+      child: Center(
+        child: Text(
+          'Somthing went wrong!',
+          textDirection: TextDirection.ltr,
+        ),
+      ),
+    );
+  }
+
+  Widget loading() {
+    return Container(
+      child: Center(
+        child: Text(
+          'Loading...',
+          textDirection: TextDirection.ltr,
+        ),
+      ),
+    );
+  }
+
+  Widget budgetApp() {
+    return MaterialApp(
+      title: 'Blume Budgeting',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Transactions'),
+        ),
+        body: Center(
+          child: new AddTransaction(
+            'LMCU',
+            DateTime.now(),
+            'Amazon',
+            5.49,
+            'Family',
+            false,
+          ),
+        ),
+      ),
+    );
+    // return Column(
+    //   children: [
+    //     AddTransaction(
+    //       'LMCU',
+    //       DateTime.now(),
+    //       'Amazon',
+    //       5.49,
+    //       'Family',
+    //       false,
+    //     ),
+    //   ],
+    // );
+  }
+}
+
+
